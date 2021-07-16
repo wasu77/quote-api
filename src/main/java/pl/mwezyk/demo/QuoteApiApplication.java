4b5b2d2537c840b765a8809a2da20372b45a1df1
mwezyk <maciek.wezyk@gmail.com>
2021-07-16 18:34:53 +0200

    Initial commit with spring boot project generated in initializr

---
+++ src/main/java/pl/mwezyk/demo/QuoteApiApplication.java
@@ -0,0 +1,13 @@
+package pl.mwezyk.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuoteApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuoteApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/mwezyk/demo/QuoteApiApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.mwezyk.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuoteApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
