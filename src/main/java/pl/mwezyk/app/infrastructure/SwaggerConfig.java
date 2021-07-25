7a115849cc65819a5c481b99da49001130d4f8a9
mwezyk <maciek.wezyk@gmail.com>
2021-07-25 21:15:03 +0200

    Moved swagger config out of quote domain

--- src/main/java/pl/mwezyk/app/infrastructure/SwaggerConfig.java
+++ src/main/java/pl/mwezyk/app/infrastructure/SwaggerConfig.java
@@ -1,4 +1,4 @@
-package pl.mwezyk.app.domain.quote.infrastructure;
+package pl.mwezyk.app.infrastructure;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
